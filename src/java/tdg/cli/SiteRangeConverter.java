0e984973dab26f08a2eff5f81daeee4575bc3041
tamuri <tamuri@gmail.com>
2012-05-23 13:36:50 +0100

    Update tutorial
    Add instructions for simulating sites and alignments using the
    mutation-selection model

---
+++ src/java/tdg/cli/SiteRangeConverter.java
@@ -0,0 +1,13 @@
+package tdg.cli;
+
+import com.beust.jcommander.IStringConverter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: atamuri
+ * Date: 13/05/2012
+ * Time: 14:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class SiteRangeConverter implements IStringConverter {
+}
---
+++ src/java/tdg/utils/Pair.java
@@ -0,0 +1,11 @@
+package tdg.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: atamuri
+ * Date: 13/05/2012
+ * Time: 15:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Pair {
+}
