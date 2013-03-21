acd6e765f0eb6811f8e4ebd5c59b86bf075604b9
Asif Tamuri <tamuri@gmail.com>
2013-03-21 22:16:43 +0000

    Put processing of checkpoint file in its own class Checkpoint files output Fitness parameters in canonical amino acid order

---
+++ src/java/tdg/CheckpointLineProcessor.java
@@ -0,0 +1,97 @@
+package tdg;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.LineProcessor;
+import pal.alignment.Alignment;
+import pal.tree.ReadTree;
+import pal.tree.Tree;
+import pal.tree.TreeParseException;
+import tdg.model.Fitness;
+import tdg.model.TDGGlobals;
+import tdg.utils.PhyloUtils;
+import tdg.utils.Triple;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.StringReader;
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * Author: Asif Tamuri (atamuri@ebi.ac.uk)
+ * Date: 21/03/2013 22:13
+ */
+public class CheckpointLineProcessor implements LineProcessor<Triple<TDGGlobals, FitnessStore, Tree>> {
+    private TDGGlobals globals;
+    private Tree tree;
+    private List<Fitness> fitnessList = Lists.newArrayList();
+    private Alignment alignment;
+
+    public CheckpointLineProcessor(Alignment alignment) {
+        this.alignment = alignment;
+    }
+
+    @Override
+    public boolean processLine(String line) throws IOException {
+
+        String[] parts;
+
+        if (line.startsWith("TDGGlobals")) {
+            // TDGGlobals{ -tau 0.1056066601361324 -kappa 3.5149103405053466 -pi 0.2702656644776448,0.2524978838686726,0.3926835680493317,0.08455288360435098 -mu 3.6173836635167165 (1/nu=0.24262321603322343)}
+            parts = line.split(" ");
+            double tau = Double.parseDouble(parts[2]);
+            double kappa = Double.parseDouble(parts[4]);
+
+            String[] piParts = parts[6].split(",");
+            double[] pi = new double[4];
+            for (int i = 0; i < pi.length; i++) {
+                pi[i] = Double.parseDouble(piParts[i]);
+            }
+
+            double mu = Double.parseDouble(parts[8]);
+
+            this.globals = new TDGGlobals(tau, kappa, pi, mu, 0);
+            System.out.printf("%s - tdg.Estimator loaded checkpoint (%s)\n", new Timestamp(System.currentTimeMillis()), this.globals.toString());
+        } else if (line.startsWith("(")) {
+
+            try {
+                this.tree = new ReadTree(new PushbackReader(new StringReader(line)));
+            } catch (TreeParseException e) {
+                e.printStackTrace();
+                throw new IOException("ERROR: Could not read checkpoint tree.");
+            }
+
+            // check the tree will work with the alignment
+            if (!PhyloUtils.isTreeAndAlignmentValid(this.tree, alignment)) {
+                throw new IOException("ERROR: Checkpoint tree does not match alignment.");
+            }
+
+            System.out.printf("%s - tdg.Estimator loaded checkpoint (tree length %s)\n", new Timestamp(System.currentTimeMillis()), PhyloUtils.getTotalTreeLength(this.tree));
+        } else if (globals != null && tree != null && (parts = line.split(",")).length == 20) {
+            // If we've successfully loaded mutational parameters and tree, and this line contains 20 comma-separated values
+
+            // Re-order the fitnesses when we read them in
+            int site = fitnessList.size() + 1;
+            List<Integer> residues = PhyloUtils.getDistinctAminoAcids(PhyloUtils.getCleanedCodons(alignment, site).values());
+
+            double[] f = new double[20];
+            for (int i = 0; i < f.length; i++) {
+                f[i] = Double.parseDouble(parts[residues.get(i)]);
+            }
+
+            fitnessList.add(new Fitness(f, true));
+        }
+
+        return true;
+    }
+
+    @Override
+    public Triple<TDGGlobals, FitnessStore, Tree> getResult() {
+        int site = 1;
+        FitnessStore fitnessStore = new FitnessStore(fitnessList.size());
+        for (Fitness f : fitnessList) fitnessStore.setFitness(site++, f);
+        System.out.printf("%s - tdg.Estimator loaded checkpoint (%s fitness vectors)\n", new Timestamp(System.currentTimeMillis()), fitnessList.size());
+
+        return Triple.of(globals, fitnessStore, tree);
+    }
+}
--- src/java/tdg/Estimator.java
+++ src/java/tdg/Estimator.java
@@ -1,27 +1,21 @@
 package tdg;
 
 import com.beust.jcommander.JCommander;
-import com.google.common.collect.Lists;
 import com.google.common.io.Files;
-import com.google.common.io.LineProcessor;
 import com.google.common.primitives.Doubles;
 import org.apache.commons.math.optimization.RealConvergenceChecker;
 import org.apache.commons.math.optimization.RealPointValuePair;
 import org.apache.commons.math.optimization.SimpleScalarValueChecker;
 import pal.alignment.Alignment;
-import pal.tree.ReadTree;
 import pal.tree.Tree;
-import pal.tree.TreeParseException;
 import tdg.model.Fitness;
 import tdg.model.TDGGlobals;
-import tdg.utils.CoreUtils;
-import tdg.utils.Pair;
-import tdg.utils.PhyloUtils;
-import tdg.utils.Triple;
+import tdg.utils.*;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -146,7 +140,8 @@ public class Estimator {
             writeResults(starttime, end - start, iteration,
                     globals, globalsOptResult.first,
                     tree, treeOptResult.first,
-                    fitnessStore, fitnessOptResult);
+                    fitnessStore, fitnessOptResult,
+                    alignment);
         }
 
         System.out.println();
@@ -158,85 +153,19 @@ public class Estimator {
     }
 
     private Triple<TDGGlobals, FitnessStore, Tree> loadCheckpoint(String checkpointFile, final Alignment alignment) {
-
-
         try {
-
-            return Files.readLines(new File(checkpointFile), Charset.defaultCharset(),
-                    new LineProcessor<Triple<TDGGlobals, FitnessStore, Tree>>() {
-                        private TDGGlobals globals;
-                        private Tree tree;
-                        private List<Fitness> fitnessList = Lists.newArrayList();
-
-                        @Override
-                        public boolean processLine(String line) throws IOException {
-
-                            String[] parts;
-
-                            if (line.startsWith("TDGGlobals")) {
-                                // TDGGlobals{ -tau 0.1056066601361324 -kappa 3.5149103405053466 -pi 0.2702656644776448,0.2524978838686726,0.3926835680493317,0.08455288360435098 -mu 3.6173836635167165 (1/nu=0.24262321603322343)}
-                                parts = line.split(" ");
-                                double tau = Double.parseDouble(parts[2]);
-                                double kappa = Double.parseDouble(parts[4]);
-
-                                String[] piParts = parts[6].split(",");
-                                double[] pi = new double[4];
-                                for (int i = 0; i < pi.length; i++) {
-                                    pi[i] = Double.parseDouble(piParts[i]);
-                                }
-
-                                double mu = Double.parseDouble(parts[8]);
-
-                                this.globals = new TDGGlobals(tau, kappa, pi, mu, 0);
-                                System.out.printf("%s - tdg.Estimator loaded checkpoint (%s)\n", new Timestamp(System.currentTimeMillis()), this.globals.toString());
-                            } else if (line.startsWith("(")) {
-
-                                try {
-                                    this.tree = new ReadTree(new PushbackReader(new StringReader(line)));
-                                } catch (TreeParseException e) {
-                                    e.printStackTrace();
-                                    throw new IOException("ERROR: Could not read checkpoint tree.");
-                                }
-
-                                // check the tree will work with the alignment
-                                if (!PhyloUtils.isTreeAndAlignmentValid(this.tree, alignment)) {
-                                    throw new IOException("ERROR: Checkpoint tree does not match alignment.");
-                                }
-
-                                System.out.printf("%s - tdg.Estimator loaded checkpoint (tree length %s)\n", new Timestamp(System.currentTimeMillis()), PhyloUtils.getTotalTreeLength(this.tree));
-                            } else if (line.startsWith("0.0") && (parts = line.split(",")).length == 20) {
-                                double[] f = new double[20];
-                                for (int i = 0; i < f.length; i++) {
-                                    f[i] = Double.parseDouble(parts[i]);
-                                }
-                                fitnessList.add(new Fitness(f, true));
-                            }
-
-                            return true;
-                        }
-
-                        @Override
-                        public Triple<TDGGlobals, FitnessStore, Tree> getResult() {
-                            int site = 1;
-                            FitnessStore fitnessStore = new FitnessStore(fitnessList.size());
-                            for (Fitness f : fitnessList) fitnessStore.setFitness(site++, f);
-                            System.out.printf("%s - tdg.Estimator loaded checkpoint (%s fitness vectors)\n", new Timestamp(System.currentTimeMillis()), fitnessList.size());
-
-                            return Triple.of(globals, fitnessStore, tree);
-                        }
-                    });
-
+            return Files.readLines(new File(checkpointFile), Charset.defaultCharset(), new CheckpointLineProcessor(alignment));
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
-
     }
 
     private void writeResults(Date date, long millis, int iteration,
                               TDGGlobals globals, double globalsOpt,
                               Tree tree, double treeOpt,
-                              FitnessStore fitnessStore, double fitnessOpt) {
+                              FitnessStore fitnessStore, double fitnessOpt,
+                              Alignment alignment) {
 
         String filename = String.format("%03d_optima.txt", iteration);
 
@@ -250,10 +179,17 @@ public class Estimator {
             bw.newLine();
             bw.write(tree.toString());
             bw.newLine();
+
+            int site = 1;
             for (Fitness f : fitnessStore) {
-                bw.write(Doubles.join(",", f.get()));
+                List<Integer> residues = PhyloUtils.getDistinctAminoAcids(PhyloUtils.getCleanedCodons(alignment, site).values());
+                double[] fitnesses = new double[20];
+                for (int i = 0; i < GeneticCode.AMINO_ACID_STATES; i++) fitnesses[i] = f.get()[residues.indexOf(i)];
+                bw.write(Doubles.join(",", fitnesses));
                 bw.newLine();
+                site++;
             }
+
             bw.newLine();
             bw.write(String.format("lnL - %s %s %s\n", globalsOpt, treeOpt, fitnessOpt));
             bw.write((millis / 1000.0) + " secs.");
